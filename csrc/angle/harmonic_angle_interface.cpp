9e6d7befd27f48e26fd1a868104372fedd7c21ad
Ericwang6 <ericwangyz@berkeley.edu>
2025-04-18 00:44:16 -0700

    fix angle errors

--- csrc/angle/harmonic_angle_interface.cpp
+++ csrc/angle/harmonic_angle_interface.cpp
@@ -1,7 +1,7 @@
 #include <pybind11/pybind11.h>
 #include <torch/library.h>
 
-TORCH_LIBRARY(torchff, m) {
+TORCH_LIBRARY_FRAGMENT(torchff, m) {
     m.def("compute_harmonic_angle_energy(Tensor coords, Tensor triplets, Tensor theta0, Tensor k) -> Tensor");
     m.def("compute_harmonic_angle_energy_grad(Tensor coords, Tensor triplets, Tensor theta0, Tensor k) -> (Tensor, Tensor, Tensor)");
 }
--- torchff/angle.py
+++ torchff/angle.py
@@ -14,7 +14,7 @@ class HarmonicAngleFunction(torch.autograd.Function):
     def backward(ctx, grad_outputs):
         coords, triplets, theta0, k = ctx.saved_tensors
         grad_outputs = torch.ops.torchff.compute_harmonic_angle_energy_grad(coords, triplets, theta0, k)
-        return grad_coords[0], None, grad_outputs[1], grad_outputs[2]
+        return grad_outputs[0], None, grad_outputs[1], grad_outputs[2]
         #return torch.Tensor(0);
 
 def compute_harmonic_angle_energy(coords: torch.Tensor, triplets: torch.Tensor, theta0: torch.Tensor, k: torch.Tensor) -> torch.Tensor:
