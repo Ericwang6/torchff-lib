fb178788a81efd985a98baa5c2be45b3cb32c378
Aalim Syed Abdullah <asa@login01.chn.perlmutter.nersc.gov>
2025-10-21 14:56:10 -0700

    Ewald CUDA implementation. Monopoles work. Dipoles and Quadrupoles give some problem

--- csrc/ewald/ewald_interface.cpp
+++ csrc/ewald/ewald_interface.cpp
@@ -3,9 +3,13 @@
 #include <torch/extension.h>
 
 TORCH_LIBRARY_FRAGMENT(torchff, m) {
-    m.def("ewald_long_range_potential(Tensor coords, Tensor box, Tensor q, Tensor p, Tensor t, Tensor all_hkl, Tensor sym_factors, Scalar alpha, Scalar rank) -> (Tensor, Tensor, Tensor)");
+  // one-shot op you’ll call from Python (returns 5 outputs)
+  m.def("ewald_long_range(Tensor coords, Tensor box, "
+        "Tensor q, Tensor p, Tensor t, int K, int rank, float alpha) "
+        "-> (Tensor, Tensor, Tensor, Tensor, Tensor)");
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-    m.doc() = "torchff multipolar Ewald CUDA extension";
+  m.doc() = "torchff multipolar Ewald CUDA extension";
 }
+
